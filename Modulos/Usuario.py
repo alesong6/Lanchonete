a86c41160b91125f95d623c3e21f88ae507ccf97
Alison <alesong6@gmail.com>
2024-06-11 11:51:33 -0300

    início do Login no Python

---
+++ Modulos/Usuario.py
@@ -0,0 +1,58 @@
+class Usuario:
+  """ Documentação da Classe no Python
+      Funções do usuário padrão da Lanchonete.
+  """
+
+  # propriedades da classe (variáveis)
+  # cadeia - string - str - "texto 1234" "1234"
+  loginInformado:str = "cliente"
+  senhaInformada:str = "1234"
+
+  # Array - matriz ou vetor nomeado
+  dadosUsuario = {
+      "loginArmazenado" : "cliente",
+      "senhaArmazenada" : "1234",
+      "nomeUsuario" : "João dos Santos",
+  }
+
+
+  # Método Construtor: executado ao instanciar a classe
+  # self refere-se à instância da classe
+  def __init__( self ):
+
+    # chamando o método logar da classe
+    self.logar()
+
+  def logar ( self ):
+
+    self.loginInformado = input( "informe o login: \n" )
+    self.senhaInformada = input( "informe o senha: \n" )
+
+    # Comparação - Condicionais - Se - if
+    # senão - else - falso
+
+    if self.loginInformado == self.dadosUsuario["loginArmazenado"]:
+      self.mostraMensagens( "Bem Vindo ao Sistema" )
+      self.exibirInfosUsuario()
+    else:
+      print( " Falha ao se conectar, tente novamente ")
+
+  def sair( self ):
+    print( "Logout do sistema" )
+
+  def exibirInfosUsuario( self ):
+    print( " Os dados do usuário são: \n Nome: \n Login: " )
+  
+  #exibir as mensagens enviadas pelo parámetro.  
+  def mostraMensagens(self, mensagem):
+    print( f"    -------------------- \n |  {mensagem}  | \n    -------------------- \n" )
+
+# Uma classe convencional precisa ser Instanciada para que seus objetos possam ser usados.
+# Instanciar uma classe é colocar uma cópia ( instância ) em uma variável ( objeto )
+# operador de atribuição =
+
+#atendente = Usuario()
+
+# usando um dos métodos
+
+#atendente.sair()
---
+++ Sistema.py
@@ -0,0 +1,7 @@
+""" 
+    Este aquivo é a entrada do sistema, ele aciona os módulos necessários.
+"""
+#importação do módulo usuario e da classe
+from Modulos.Usuario import Usuario
+
+usuario = Usuario()
